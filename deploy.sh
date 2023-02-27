e627d047f4a0c4072c9f10d3df80a855a4d8085f
edit <uic.shivanijaswal@gmail.com>
2023-02-27 20:07:09 +0000

    Last commit2

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install --lts
-##sudo npm install -g pm2
+npm install -g pm2
 pm2 stop example_app
 cd ExampleApplication/
-#npm install
+npm install
 pm2 start ./bin/www --name example_app
